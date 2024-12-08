17a1d2f0ed6dbe64f562de3b16adbd44d610e9c9
Red <redouanehalmi20@gmail.com>
2024-12-08 00:46:45 +0000

    Le test apres Assertj

--- src/test/java/com/red/calculator/CalculatorTest.java
+++ src/test/java/com/red/calculator/CalculatorTest.java
@@ -2,6 +2,7 @@ package com.red.calculator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -9,7 +10,8 @@ class CalculatorTest {
     @Test
     void add() {
         Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3), "2+3 devrait donner 5 comme resultat");
+        assertThat(calculator.add(2, 3)).isEqualTo(5);
+        assertThat(calculator.add(2, 13)).isEqualTo(15);
         assertEquals(0, calculator.add(-1, 1),"-1+1 devrait donner 0 comme resultat ");
         assertEquals(-5, calculator.add(-2, -3),"-2-3 devrait donner - 5 comme resultat");
     }
