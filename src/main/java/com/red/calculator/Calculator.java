ced5d963a0531759270f3947dca99e10f66c3e90
Red <redouanehalmi20@gmail.com>
2024-12-08 01:07:40 +0000

    Changement en static

--- src/main/java/com/red/calculator/Calculator.java
+++ src/main/java/com/red/calculator/Calculator.java
@@ -2,11 +2,14 @@ package com.red.calculator;
 
 public class Calculator {
 
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
 
-    public int divide(int opG, int opD) {
-        return opG / opD;
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division par zéro");
+        }
+        return a / b;
     }
 }
--- src/test/java/com/red/calculator/CalculatorTest.java
+++ src/test/java/com/red/calculator/CalculatorTest.java
@@ -4,37 +4,34 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-    Calculator calculator;
+
     @BeforeEach
-    public void setUP(){
-        calculator=new Calculator();
+    public void setUp() {
         System.out.println("SetUp");
     }
+
     @Test
     void add() {
-        assertThat(calculator.add(2, 3)).isEqualTo(5);
-        assertThat(calculator.add(2, 13)).isEqualTo(15);
-        assertEquals(0, calculator.add(-1, 1),"-1+1 devrait donner 0 comme resultat ");
-        assertEquals(-5, calculator.add(-2, -3),"-2-3 devrait donner - 5 comme resultat");
+        assertThat(Calculator.add(2, 3)).isEqualTo(5);
+        assertThat(Calculator.add(2, 13)).isEqualTo(15);
+        assertEquals(0, Calculator.add(-1, 1), "-1 + 1 devrait donner 0 comme résultat");
+        assertEquals(-5, Calculator.add(-2, -3), "-2 + (-3) devrait donner -5 comme résultat");
     }
 
     @Test
-    void divide() { 
-        assertEquals(1,calculator.divide(3,2),"devrait donner 1 comme resultat");
-        assertEquals(5,calculator.divide(10,2),"devrait donner 5 comme resultat");
-        assertEquals(3,calculator.divide(6,2),"devrait donner 3 comme resultat");
-
-        assertThrows(ArithmeticException.class, () -> calculator.divide(1, 0), "la division par 0 est impossible");
+    void divide() {
+        assertEquals(1, Calculator.divide(3, 2), "Devrait donner 1 comme résultat");
+        assertEquals(5, Calculator.divide(10, 2), "Devrait donner 5 comme résultat");
+        assertEquals(3, Calculator.divide(6, 2), "Devrait donner 3 comme résultat");
 
     }
+
     @AfterEach
-        public void tearDown(){
-        calculator=null;
+    public void tearDown() {
         System.out.println("TearDown");
     }
 }
