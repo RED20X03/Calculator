6880a2114d5aaeebf5e4848c2b640512860841e5
Red <redouanehalmi20@gmail.com>
2024-12-15 18:13:27 +0000

    Q2

--- src/test/java/com/red/calculator/CalculatorTest.java
+++ src/test/java/com/red/calculator/CalculatorTest.java
@@ -4,15 +4,16 @@ package com.red.calculator;
 import org.junit.jupiter.api.Test;
 
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     @Test
 void add() {
     Calculator calculator=new Calculator();
-    assertEquals(5,calculator.add(2,3),"2+3 devrait donner 5 comme resultat");
-    assertEquals(0,calculator.add(-1,1),"-1+1 devrait donner 0 comme resultat");
-    assertEquals(-5,calculator.add(-2,-3),"-2-3 devrait donner -5 comme resultat");
+    assertThat(calculator.add(2,3)).isEqualTo(5);
+        assertThat(calculator.add(10,3)).isEqualTo(13);
+        assertThat(calculator.add(20,3)).isEqualTo(23);
 }
 
     @Test
