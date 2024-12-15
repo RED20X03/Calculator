a433759fcfe4c39759d6587c777c0e1fdc33b00f
Red <redouanehalmi20@gmail.com>
2024-12-15 18:19:54 +0000

    Q3

--- src/test/java/com/red/calculator/CalculatorTest.java
+++ src/test/java/com/red/calculator/CalculatorTest.java
@@ -1,6 +1,8 @@
 package com.red.calculator;
 
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -8,12 +10,19 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator;
+    @BeforeEach
+    public void setUP(){
+        calculator=new Calculator();
+        System.out.println("SetUp");
+    }
+
     @Test
 void add() {
-    Calculator calculator=new Calculator();
     assertThat(calculator.add(2,3)).isEqualTo(5);
-        assertThat(calculator.add(10,3)).isEqualTo(13);
-        assertThat(calculator.add(20,3)).isEqualTo(23);
+        assertThat(calculator.add(2,13)).isEqualTo(15);
+        assertEquals(0,calculator.add(-1,1),"-1+1 devrait donner 0 comme resultat");
+        assertEquals(-5,calculator.add(-2,-3),"-2-3 devrait donner -5 comme resultat");
 }
 
     @Test
@@ -22,6 +31,13 @@ void add() {
         assertEquals(1,calculator.divide(3,2)," devrait donner 1 comme resultat");
         assertEquals(5,calculator.divide(10,2)," devrait donner 5 comme resultat");
         assertEquals(3,calculator.divide(6,2)," devrait donner 3 comme resultat");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        calculator=null;
+        System.out.println("TearDown");
+
     }
     }
 
