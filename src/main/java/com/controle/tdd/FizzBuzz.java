f16a142f3b588201e8cdb69a00f6db5a3c374db6
Red <redouanehalmi20@gmail.com>
2024-12-19 16:54:32 +0000

    Cycle 3 : Test échoué pour FizzBuzz.de(n)

--- src/main/java/com/controle/tdd/FizzBuzz.java
+++ src/main/java/com/controle/tdd/FizzBuzz.java
@@ -2,6 +2,12 @@ package com.controle.tdd;
 
 public class FizzBuzz {
     public static String de(int number) {
-        return String.valueOf(number); // Code corrigé
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(number);
     }
 }
--- src/test/java/com/controle/tdd/FizzBuzzTest.java
+++ src/test/java/com/controle/tdd/FizzBuzzTest.java
@@ -5,9 +5,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
-    // Test pour vérifier que FizzBuzz.de(1) retourne "1"
     @Test
     void shouldReturnNumberWhenNotDivisibleBy3Or5() {
         assertThat(FizzBuzz.de(1)).isEqualTo("1");
+
+    }
+    @Test
+    void shouldReturnFizzWhenDivisibleBy3() {
+        assertThat(FizzBuzz.de(3)).isEqualTo("Fizz");
+    }
+    @Test
+    void shouldReturnBuzzWhenDivisibleBy5() {
+        assertThat(FizzBuzz.de(5)).isEqualTo("Buzz");
+    }
+    @Test
+    void shouldReturnFizzBuzzWhenDivisibleBy3And5() {
+        assertThat(FizzBuzz.de(15)).isEqualTo("FizzBuzz"); // Test échouera
     }
 }
