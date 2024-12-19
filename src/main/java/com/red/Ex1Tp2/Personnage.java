d5da4530c7574151bbeaa3bcbead0d9c09c49436
Red <redouanehalmi20@gmail.com>
2024-12-19 13:28:37 +0000

    Refactorisation : améliorer la lisibilté de tourner()

--- src/main/java/com/red/Ex1Tp2/Personnage.java
+++ src/main/java/com/red/Ex1Tp2/Personnage.java
@@ -9,12 +9,15 @@ public class Personnage {
     //Modification de la méthode tourner pour gérer le cas d'un seul quart de tour
     public Orientation tourner(int fois) {
         Orientation[] orientations = Orientation.values();
+        // Calcule le nouvel index en prenant en compte les dépassements
         int index = (orientation.ordinal() + fois) % orientations.length;
 
+        // Ajuste les indices négatifs pour rester dans les limites du tableau
         if (index < 0) {
-            index += orientations.length; // Gère les indices négatifs
+            index += orientations.length;
         }
 
+        // Met à jour l'orientation actuelle
         this.orientation = orientations[index];
         return this.orientation;
     }
