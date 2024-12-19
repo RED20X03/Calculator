efd18d4372150da9704bdebebf9f53461e58f9f3
Red <redouanehalmi20@gmail.com>
2024-12-19 18:10:46 +0000

    Tester le main

---
+++ src/test/java/com/controle/tdd/Main.java
@@ -0,0 +1,10 @@
+package com.controle.tdd;
+
+public class Main {
+    public static void main(String [] args){
+        for (int i=0;i<=20;i++){
+            System.out.println(FizzBuzz.de(i));
+        }
+
+    }
+}
