650acd89853a49e6bfdbfefd8eb4e2e339279e3c
Red <redouanehalmi20@gmail.com>
2024-12-19 17:20:35 +0000

    Test paramétré pour tester les nombres de 1 à 20

--- src/test/java/com/controle/tdd/FizzBuzzTest.java
+++ src/test/java/com/controle/tdd/FizzBuzzTest.java
@@ -1,25 +1,36 @@
 package com.controle.tdd;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
-    @Test
-    void shouldReturnNumberWhenNotDivisibleBy3Or5() {
-        assertThat(FizzBuzz.de(1)).isEqualTo("1");
-
-    }
-    @Test
-    void shouldReturnFizzWhenDivisibleBy3() {
-        assertThat(FizzBuzz.de(3)).isEqualTo("Fizz");
-    }
-    @Test
-    void shouldReturnBuzzWhenDivisibleBy5() {
-        assertThat(FizzBuzz.de(5)).isEqualTo("Buzz");
-    }
-    @Test
-    void shouldReturnFizzBuzzWhenDivisibleBy3And5() {
-        assertThat(FizzBuzz.de(15)).isEqualTo("FizzBuzz"); 
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1",
+            "2, 2",
+            "3, Fizz",
+            "4, 4",
+            "5, Buzz",
+            "6, Fizz",
+            "7, 7",
+            "8, 8",
+            "9, Fizz",
+            "10, Buzz",
+            "11, 11",
+            "12, Fizz",
+            "13, 13",
+            "14, 14",
+            "15, FizzBuzz",
+            "16, 16",
+            "17, 17",
+            "18, Fizz",
+            "19, 19",
+            "20, Buzz"
+    })
+    void shouldReturnCorrectFizzBuzzOutput(int input, String expected) {
+        assertThat(FizzBuzz.de(input)).isEqualTo(expected);
     }
 }
